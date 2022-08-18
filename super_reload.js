8eda8edcec45843e74c91f2bd891f6c933c57b77
pscore23 <103930835+pscore23@users.noreply.github.com>
2022-08-18 20:42:40 +0900

    Update super_reload.js

--- super_reload.js
+++ super_reload.js
@@ -1,3 +1,3 @@
-completion(window.location.reload(true));  // 調べてみたらデマらしい
+completion(window.location.reload(true));  // 調べてみたらデマらしい (知らんけど)
 
-window.location.href = window.location.href  // これは出来るらしい
+window.location.href = window.location.href  // これは出来るらしい (知らんけど)
